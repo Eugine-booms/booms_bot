2c23d5fbaef38b5c3f6a35bc3dfecc5bce5d5ef7
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-08-02 19:13:53 +0300

    bugfix Вроде работает погода по одному городу

--- Model/Bot.cs
+++ Model/Bot.cs
@@ -14,43 +14,21 @@ namespace TelegramMyFirstBot.Model.Commands
 
         public static IReadOnlyList<Command> Commands => commandsList.AsReadOnly();
         public static TelegramBotClient GetClient() => client;
-        public static void OnMessageHandler(object sender, MessageEventArgs e)
+        public static async void OnMessageHandler(object sender, MessageEventArgs e)
         {
             var message = e.Message;
             Console.WriteLine($"[log]: Пришло сообщение от : {message.From.FirstName} с текстом {message.Text}");
           
             foreach (var command in commandsList)
             {
-                if (command.Contains(message.Text.ToLower()))
+                if (message.Text!= null && command.Contains(message.Text.ToLower()))
                 {
                     command.Execute(message, client);
                     break;
                 }
             }
-
-            //if (e.Message != null && e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && !string.IsNullOrEmpty(e.Message.Text))
-            //{
-            //    try
-            //    {
-            //        if (e.Message.Text.ToLower().Contains("привет"))
-            //         Bot.GetClient().SendTextMessageAsync(e.Message.Chat.Id, "Привет мой кожаный друг");
-                    
-            //        if (e.Message.Text.ToLower().Contains("как дела"))
-            //        {
-            //            Bot.GetClient().SendTextMessageAsync(e.Message.Chat.Id, "еще живой");
-            //        }
-            //        else
-            //        {
-            //            Bot.GetClient().SendTextMessageAsync(e.Message.Chat.Id, "Че надо");
-            //        }
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine(ex.ToString());
-            //    }
-
-            //}
         }
+
         public static void Init()
         {
             client = new TelegramBotClient(AppSettings.Key) {Timeout = TimeSpan.FromSeconds(10) };
@@ -58,6 +36,7 @@ namespace TelegramMyFirstBot.Model.Commands
             Console.WriteLine($"BotID: { me.Id}\n BotName:{me.FirstName}");
             commandsList = new List<Command>();
             commandsList.Add(new HelloComand());
+            commandsList.Add(new WeatherCommand());
             //TODO: Add more commands
         }    
     }
---
+++ Model/WeatherResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramMyFirstBot.Model
+{
+    internal class WeatherResponse
+    {
+        public TemperatureInfo Main { get; set; }
+
+        public string Name { get; set; }
+    }
+}
